20d2a52b06567405388e44a61c8145f067136b6d
Mike Stewart <thewonderidiot@gmail.com>
2026-03-15 18:35:32 -0700

    Add script to interface FPGA telemetry to OpenC3

---
+++ scripts/saturn_tlm_server.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+from serial.tools import list_ports
+import serial
+import time
+import struct
+import socket
+import sys
+
+SYNC = b'\xA5\x5A'
+
+def check_parity(tag, word, parity):
+    word = (tag << 26) | word
+    p = 0
+    for i in range(38):
+        if word & (1 << i):
+            p ^= 1
+
+    return parity != p
+
+# Locate and connect to the Cmod
+port = None
+devices = list_ports.comports()
+for dev in devices:
+    if dev.vid == 0x0403 and dev.pid == 0x6010:
+        port = dev.device
+        break
+
+s = serial.Serial(port, 115200, timeout=0.01)
+
+# Open up a socket for OpenC3
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('172.17.0.1', 8081))
+sock.settimeout(0.01)
+sock.listen(1)
+conn = None
+
+start = time.time()
+data = b''
+while True:
+    # Check for OpenC3 connections
+    if conn is None:
+        try:
+            conn,addr = sock.accept()
+            print('Connected!')
+        except:
+            pass
+
+    # Read in LVDC telemetry
+    data += s.read(36)
+    if not data:
+        continue
+
+    # Search for the sync pattern, and sync to it
+    if data[0] != 0xA5:
+        data = data[data.find(0xA5):]
+
+    num_frames = len(data)//6
+    for i in range(num_frames):
+        # Reconstitute the tag and word from the transmission format
+        bs = data[i*6+1:i*6+6]
+        tag = ((bs[0] & 0xE0) << 4) | ((bs[1] & 0x3F) << 3) | ((bs[2] & 0xC0) >> 5) | ((bs[2] & 0x10) >> 4)
+        word = (((bs[0] & 0x0F) << 2) | ((bs[1] & 0xC0) >> 6) | ((bs[2] & 0x0F) << 12) |
+                ((bs[3] & 0xFC) << 4) | ((bs[3] & 0x3) << 24) | (bs[4] << 16))
+        parity = (bs[0] & 0x10) >> 4
+
+        # Check the validity bit. If set, data is possible corrupt.
+        if bs[2] & 0x20:
+            continue
+
+        # Check the parity of the frame.
+        if not check_parity(tag, word, parity):
+            continue
+
+        if tag & 0x100:
+            # LVDC telemetry: transmit the entire tag and word
+            packet = SYNC + struct.pack('>HI', tag, word)
+            print(f'{tag:04o}    {word:09o}')
+        else:
+            # LVDA telemetry: extract the RTC timestamp bits from the tag
+            # and send the two separately.
+            rtc = ((tag >> 9) & 0o7) | ((tag << 3) & 0o10)
+            tag = (tag & 0o776) >> 1
+            packet = SYNC + struct.pack('>HBI', tag, rtc, word)
+            print(f'{tag:04o} {rtc:02o} {word:09o}')
+
+        sys.stdout.flush()
+
+        # Transmit packetized data to OpenC3
+        if conn is not None:
+            try:
+                conn.sendall(packet)
+            except:
+                conn = None
+
+    data = data[num_frames*6:]
